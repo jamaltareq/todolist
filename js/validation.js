da57f183728d2e34c9cfbc41cdb4def245231b77
gamal <110998794+jamaltareq@users.noreply.github.com>
2023-12-28 12:35:05 +0200

    Update validation.js

--- js/validation.js
+++ js/validation.js
@@ -37,7 +37,7 @@ if(dtnCreate != null){
             })
             console.log(objectCreate);
             localStorage.setItem("createAcount", JSON.stringify(objectCreate))
-            location.href = "../todolist/sign.html";
+            location.href = "https://jamaltareq.github.io/todolist/sign.html";
         }
     
     })
@@ -49,5 +49,5 @@ let emailsign = document.getElementById("emailsign")
 let passwordsign = document.getElementById("passwordsign")
 let btn_sign = document.getElementById("btn_sign")
 btn_sign?.addEventListener('click', () => {
-    location.href= "../todolist/page.html"
+    location.href= "https://jamaltareq.github.io/todolist/page.html"
 })
